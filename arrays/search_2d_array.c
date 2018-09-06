fe8bde6894bf01c6c62865be9db5a9375f550b77
nahush8 <nahushg@vt.edu>
2018-09-05 19:39:03 -0700

    Adding binary search

--- arrays/search_2d_array.c
+++ arrays/search_2d_array.c
@@ -3,7 +3,7 @@
 #include<stdbool.h>
 
 #define ROWS 3
-#define COLS 3
+#define COLS 1
 
 bool search(int *arr, int value)
 {
@@ -18,16 +18,37 @@ bool search(int *arr, int value)
 	}
 
 	return false;
-}(
+}
 
 bool binary_search(int *arr, int value)
 {
-	
+	int start = 0;
+	int end = ROWS * COLS;
+	int mid;
+
+	while(start <= end)
+	{
+		mid = (start + end) /2;
+		if(value == *(arr + mid))
+			return true;
+		else if(value > *(arr + mid)) 
+		{
+			start = mid + 1;
+		}
+		else if(value < *(arr + mid))
+		{
+			end = mid - 1;
+		}
+	}
+	return false;
 }
 
 int main(void)
 {
-	int arr[][3] ={{1,3,5},{7,8,9},{11,13,15}};
-	printf("\n%s\n", (search((int*)arr,13) ? "true" : "false")); 
+	//int arr[][3] ={{1,3,5},{7,8,9},{11,13,15}};
+	int arr[3][1] ={1,3,15};
+	//int arr[][3] ={{1,3,5},{7,8,9},{11,13,15}};
+	//int arr[][3] ={{1,3,5},{7,8,9},{11,13,15}};
+	printf("\n%s\n", (binary_search((int*)arr,15) ? "true" : "false")); 
 	return 0;	
 }
